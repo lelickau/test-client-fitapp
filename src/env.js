687b3f23d64e97bc0556dd84abe80027e07d4550
elena_kau <lelickau@yandex.ru>
2021-12-22 14:13:39 +0200

    test 3

---
+++ src/env.js
@@ -0,0 +1,14 @@
+// API FOOD
+export const APP_ID = 'a2a54800';
+export const API_KEY = 'afc67ff1d7b58baa75dfdfe65774d387';
+export const API_FOOD_URL = `https://api.edamam.com/api/food-database/v2/parser?app_id=${APP_ID}&app_key=${API_KEY}`;
+
+// MY API
+export const SERVER_URL = `https://murmuring-refuge-58184.herokuapp.com/`;
+export const API_URL_AUTH = `https://murmuring-refuge-58184.herokuapp.com/api/auth/`;
+export const MY_API_URL_FOODS = `https://murmuring-refuge-58184.herokuapp.com/api/foods/`;
+export const API_URL_NOTES = `https://murmuring-refuge-58184.herokuapp.com/api/notes/`;
+
+
+
+
